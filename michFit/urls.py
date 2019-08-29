a8df03ab1e19d01d30054739dcdbad86dc753841
tkamoua <tkamoua@umich.edu>
2019-08-28 23:39:41 -0400

    added email functionality

--- michFit/urls.py
+++ michFit/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('cart_add/<item_id>/',views.cart_add, name='cart_add'),
     path('cart/view/<item_id>/',views.cart_view, name='cartview'),
-    path('cartdelete/<item_id>/',views.cartdelete, name='cartdelete')
+    path('cartdelete/<item_id>/',views.cartdelete, name='cartdelete'),
+    path('sendemail/',views.send_email,name='sendemail')
 ]
--- michFit/views.py
+++ michFit/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import smtplib
 
 from django.http import HttpResponse, Http404
 from .models import MenuItem
@@ -18,6 +18,7 @@ def home(request):
 
 def cartdelete(request, item_id):
     try:
+        print(cart)
         cart.remove(MenuItem.objects.get(pk=item_id))
         totalCalories = 0
         totalProtein=0
@@ -26,6 +27,7 @@ def cartdelete(request, item_id):
             totalProtein+=item.protein
         return render(request, 'michFit/cart.html',{'cart': cart, 'totalCalories': totalCalories, 'totalProtein': totalProtein} )
     except:
+        print(cart)
         totalCalories = 0
         totalProtein=0
         for item in cart:
@@ -51,3 +53,23 @@ def cart_view(request,item_id):
         totalCalories += item.calories
         totalProtein+=item.protein
     return render(request, 'michFit/cart.html',{'cart': cart, 'totalCalories': totalCalories, 'totalProtein': totalProtein} )
+
+def send_email(request):
+    print('send email view')
+    names=[]
+    for item in cart:
+        names.append(item.name)
+    print(names)
+    smtpObj = smtplib.SMTP('smtp.gmail.com',587)
+    print('smtplib.SMTP')
+    smtpObj.ehlo()
+    print('smtpObj.ehlo')
+    smtpObj.starttls()
+    print('starttls')
+    smtpObj.login('tkamoua@gmail.com',input())
+
+    print('Login success')
+    smtpObj.sendmail('tkamoua@gmail.com','tkamoua@umich.edu','Subject: Menu. \n\n %s' %(names))
+    print('sent email')
+    smtpObj.quit()
+    return HttpResponse("Email sent")
